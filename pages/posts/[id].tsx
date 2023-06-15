dd0a3b32fbac35c2676d33e4b0f215f1a12dda5b
t-hirata74 <t.hirata.0463@gmail.com>
2023-06-16 00:44:19 +0900

    詳細ページのgetStaticPathsを作成中

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,4 +1,41 @@
+import { Post } from "src/types";
 import React from 'react'
+
+type Props = {
+    post: Post;
+};
+
+// pages/posts/[id].tsx
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:3001/api/v1/posts");
+  const posts: Post[] = await res.json()
+
+  const paths = posts.map((post) => ({
+    params: { id: post.id.toString()},
+  }))
+
+  return {
+    paths: [
+      // Object variant:
+      {params: {id: "/posts/"} },
+    ],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({params}: {params: {id: string}}) {
+  const res = await fetch(`http://localhost:3001/api/v1/posts/${params.id}`);
+  const post = await res.json()
+
+  console.log(post)
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60,
+  };
+}
 
 const Post = () => {
   return <div>詳細ページです</div>
