e1a45f5f84fb3a1e8f159c5f68a06ae61fe04da4
t-hirata74 <t.hirata.0463@gmail.com>
2023-06-26 17:31:33 +0900

    編集機能の追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,9 @@ export default function Home({ posts }: Props) {
                 <h2>{post.title}</h2>
               </Link>
               <p>{post.content}</p>
-              <button className={styles.editButton}>Edit</button>
+              <Link href={`/edit-post/${post.id}`}>
+                <button className={styles.editButton}>Edit</button>
+              </Link>
               <button className={styles.deleteButton}>Delete</button>
             </div>
           ))}
