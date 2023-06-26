ca8a82ee18ff4b71adf16e0f52aab78b0a7b6ede
t-hirata74 <t.hirata.0463@gmail.com>
2023-06-26 16:56:47 +0900

    登録後のレンダリング

--- pages/create-post.tsx
+++ pages/create-post.tsx
@@ -1,10 +1,12 @@
 import React, { ChangeEvent, FormEvent, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import axios from 'axios';
+import { useRouter } from 'next/router';
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("")
+  const router = useRouter();
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -15,6 +17,8 @@ const CreatePost = () => {
         title: title,
         content: content,
       });
+
+      router.push("/")
     } catch (err) {
       alert('投稿に失敗しました');
     }
